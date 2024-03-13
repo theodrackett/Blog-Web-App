183011d57167905c95f047c12a0423d24c6f74eb
Theo Drackett <37917495+theodrackett@users.noreply.github.com>
2024-03-13 16:59:09 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("./index.ejs", {blogs: blogArray});
+})
+
+app.get("/create", (req, res) => {
+    res.render("./createblog.ejs");
+})
+
+const blogArray = ["I love to blog! Mostly because I love to write and blogging is all about writing.", 
+"Okay, this is my second blog. And guess what? It is still about writing. Mmm, I can't help but wonder if my third blog still involves writing",
+"You guessed it! My third blog still involves writing. Jeeze! How long will this go on?", "I guess I'll have to just keep blogging to ding out"];
+
+app.listen(port, () => {
+    console.log(`I am listening on port ${port}`);
+})
