cb719eb928f7cc3966fa2cbe4eff916ba47d30c9
Theo Drackett <37917495+theodrackett@users.noreply.github.com>
2024-03-20 11:08:35 -0700

    Add create blog functionality

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.get("/create", (req, res) => {
     res.render("./createblog.ejs");
 })
 
+app.post("/create", (req, res) => {
+    blogArray.push(req.body.blog);
+    res.redirect("/");
+})
+
 const blogArray = ["I love to blog! Mostly because I love to write and blogging is all about writing.", 
 "Okay, this is my second blog. And guess what? It is still about writing. Mmm, I can't help but wonder if my third blog still involves writing",
 "You guessed it! My third blog still involves writing. Jeeze! How long will this go on?", "I guess I'll have to just keep blogging to ding out"];
